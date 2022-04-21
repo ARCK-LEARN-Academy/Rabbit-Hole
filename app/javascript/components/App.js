e5f7c1ee8217fbf5332a5b4adae998f069ba34ae
Curtis A. Liu <Curtisaliu@gmail.com>
2022-04-21 16:14:19 -0700

    home navigation, incomplete

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -10,16 +10,18 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-          <Header />
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/burrow/:id" component={ShowBurrow} />
-          <Route path="/post/:id" component={Post} />
+          {/* <Route path="/burrowshow/:id" component={ShowBurrow} />
+          <Route path="/burrow/new" component={BurrowNew} /> */}
+
+          {/* <Route path="/post/:id" component={Post} />
           <Route path="/user/:id" component={UserProfile} />
-          <Route path="/about" component={AboutUs} />
+          <Route path="/about" component={AboutUs} /> */}
         </Switch>
-          <Paginate />
-          <Footer />
+        <Paginate />
+        <Footer />
       </Router>
     );
   }
