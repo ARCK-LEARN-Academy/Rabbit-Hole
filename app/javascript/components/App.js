2b40d7eea4929c7b32eb9ac266714e951a18f759
Alex Ranga <alexisranga@gmail.com>
2022-04-21 13:20:02 -0700

    landing page with header, footer, and pagination

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Home from './pages/Home'
+import Paginate from "./components/Paginate"
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 import {
   BrowserRouter as Router,
@@ -14,6 +16,8 @@ class App extends React.Component {
       <>
       <Header />
       <Home />
+      <Paginate />
+      <Footer />
       </>
     );
   }
