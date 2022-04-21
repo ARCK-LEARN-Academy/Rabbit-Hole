236f9c094d3d2b80c2b8827aac177859076c42de
Alex Ranga <alexisranga@gmail.com>
2022-04-21 13:04:04 -0700

    landing page with header

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Home from './pages/Home'
+import Header from './components/Header'
 
 import {
   BrowserRouter as Router,
@@ -10,7 +11,10 @@ import {
 class App extends React.Component {
   render() {
     return (
+      <>
+      <Header />
       <Home />
+      </>
     );
   }
 }
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import BurrowCards from '../components/BurrowCards'
 
 class Home extends Component {
     render() {
         return (
+            <>
             <h1>Pizza</h1>
+            <BurrowCards />
+            </>
         )
     }
 }
