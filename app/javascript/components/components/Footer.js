57193c07b7743403a5b62a565df8daa688b09ba1
Alex Ranga <alexisranga@gmail.com>
2022-04-21 13:30:02 -0700

    landing page with header, footer, and pagination; indentation corrected

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -1,15 +1,12 @@
-import React, {Component} from "react";
-
+import React, { Component } from "react";
 
 class Footer extends Component {
-    render() {
-        return(
-            <>
-            <h3> Footer </h3>
-
-            </>
-
-        )
-    }
+  render() {
+    return (
+      <>
+        <h3> Footer </h3>
+      </>
+    );
+  }
 }
-export default Footer
+export default Footer;
